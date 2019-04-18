d68d3db3565274ffdc3df7f81afc528584f5a785
Sri Harshal Parimi <psriharshal7@gmail.com>
2019-04-18 18:38:34 -0500

    Updated config for extracting essentia features

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -4,5 +4,5 @@ HOME_DIR = os.path.expanduser('~')
 DATA_DIR = os.path.join(HOME_DIR, 'private/CS760/Project/Data')
 AUDIO_DIR = os.path.join(DATA_DIR, 'fma_small')
 MEL_SPEC_DIR = os.path.join(DATA_DIR, 'mel_spec')
-
+ESS_DIR = os.path.join(DATA_DIR, 'essentia_features')
 N_MELS = 128
